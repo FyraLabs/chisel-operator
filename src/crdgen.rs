4ccfc814d1c8b54faf164d575cc904327b853903
Lleyton Gray <lleyton@fyralabs.com>
2023-12-19 20:26:51 -0800

    chore: make crdgen replace the crd files

--- src/crdgen.rs
+++ src/crdgen.rs
@@ -1,4 +1,6 @@
 use kube::CustomResourceExt;
+use std::fs::File;
+use std::io::prelude::*;
 
 mod cloud;
 mod daemon;
@@ -9,10 +11,21 @@ mod ops;
 // todo: Make this a cargo xtask, maybe?
 
 fn main() {
-    println!("{}", serde_yaml::to_string(&ops::ExitNode::crd()).unwrap());
-    println!("---");
-    println!(
-        "{}",
-        serde_yaml::to_string(&ops::ExitNodeProvisioner::crd()).unwrap()
-    )
+    let mut exit_node_provisioner = File::create("deploy/crd/exit-node-provisioner.yaml").unwrap();
+    exit_node_provisioner
+        .write_all(
+            serde_yaml::to_string(&ops::ExitNodeProvisioner::crd())
+                .unwrap()
+                .as_bytes(),
+        )
+        .unwrap();
+
+    let mut exit_node = File::create("deploy/crd/exit-node.yaml").unwrap();
+    exit_node
+        .write_all(
+            serde_yaml::to_string(&ops::ExitNode::crd())
+                .unwrap()
+                .as_bytes(),
+        )
+        .unwrap();
 }
